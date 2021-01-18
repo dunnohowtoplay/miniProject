2e8cc9d4b2f0dd7f19ac3590c0f51fa5a30de4cf
dunnohowtoplay <fauzanpartager@gmail.com>
2021-01-18 18:47:13 +0700

    fix bugs

--- Python/tictactoe.py
+++ Python/tictactoe.py
@@ -6,8 +6,6 @@ board = [
     '-', '-', '-'
 ]
 
-game_running = True
-
 # board condition
 
 
@@ -18,13 +16,13 @@ def board_condition():
 
 # npc turn
 def npc():
-    global game_running
+    #global game_running
     arr = []
     for x in range(len(board)):
         if board[x] == '-':
             arr.append(x)
     if len(arr) == 0:
-        game_running = False
+        return False
     else:
         board[random.choice(arr)] = 'O'
 
@@ -40,15 +38,10 @@ def controller():
     board[int(pos)-1] = 'X'
 
 # winning condition
-
-
-def check_winner():
+def win_condition():
     # win => all winning possibility
     win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
             [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    # Draw
-    if '-' not in board:
-        return 'D'
 
     for x in win:
         # X win condition
@@ -57,26 +50,43 @@ def check_winner():
         # O win condition
         if all(board[y] == 'O' for y in x):
             return 'O'
+        # Draw
+        if not(all(board[y] == 'X' for y in x) and all(board[y] == 'O' for y in x)) and '-' not in board:
+            return 'D'
+
+#check winner
+def check_winner():
+    if win_condition() == 'X':
+        board_condition()
+        print('X Win!')
+        return False
+    elif win_condition() == 'O':
+        board_condition()
+        print('O Win!')
+        return False
+    elif win_condition() == 'D':
+        board_condition()
+        print('Draw')
+        return False
+    else:
+        return True
+
 
 #start the game
 def start_game():
-    global game_running
-    while game_running:
+    while True:
         board_condition()
+        if check_winner() == False:
+            break
         controller()
-        if check_winner() == 'X':
-            board_condition()
-            print('X Win!')
-            game_running = False
-        if check_winner() == 'O':
-            board_condition()
-            print('O Win!')
-            game_running = False
-        if check_winner() == 'D':
-            board_condition()
-            print('Draw')
-            game_running = False
-        npc()
+        if check_winner() == False:
+            break
+        if npc() == False:
+            break
+        if check_winner() == False:
+            break
+        
+        
 
 #reset game board        
 def reset():
@@ -85,16 +95,12 @@ def reset():
 
 
 while True:
-    if game_running == True:
-        start_game()
-    else:
-        ask = input('Play Again ? (y/n)')
-        while ask not in ('yYnN') or ask == '':
-            ask = input('Invalid input, Play Again ? (y/n)')
+    start_game()
+    ask = input('Play Again ? (y/n)')
+    while ask not in ('yYnN') or ask == '':
+        ask = input('Invalid input, Play Again ? (y/n)')
 
-        if ask in ('yY'):
-            game_running = True
-            reset()
-            start_game()
-        else:
-            break
+    if ask in ('yY'):
+        reset()
+    else:
+        break
